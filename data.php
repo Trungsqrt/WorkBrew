b3de68d228777346dd07eebdd70b3bb8a4cdf010
unknown <nguyennhumay901@gmail.com>
2023-08-05 17:37:39 +0700

    create fake data

---
+++ data.php
@@ -0,0 +1,129 @@
+<?php
+
+return [
+  [
+    'title' => 'UI/UX Designer',
+    'tags' => 'ui design, ux design, prototyping',
+    'company' => 'Creative Minds Agency',
+    'location' => 'San Francisco, CA',
+    'email' => 'designer@creativeminds.com',
+    'website' => 'https://www.creativeminds.com',
+    'description' => 'Creative Minds Agency is hiring a talented UI/UX Designer to join our team in San Francisco. As a UI/UX Designer, you will be responsible for creating visually appealing and intuitive user interfaces. Proficiency in UISure! Here are 20 fake data entries in the format of developer recruitment'
+  ],
+  [
+    'title' => 'Laravel Senior Developer',
+    'tags' => 'laravel, javascript',
+    'company' => 'Acme Corp',
+    'location' => 'Boston, MA',
+    'email' => 'developer1@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is seeking a skilled Laravel Senior Developer to join our team in Boston, MA. The ideal candidate should have expertise in Laravel framework and JavaScript. If you are passionate about web development and have a strong understanding of backend technologies, apply now!'
+  ],
+  [
+    'title' => 'Frontend Developer',
+    'tags' => 'html, css, javascript',
+    'company' => 'Acme Corp',
+    'location' => 'New York, NY',
+    'email' => 'developer2@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is looking for a talented Frontend Developer to join our team in New York, NY. As a Frontend Developer, you will be responsible for implementing user interfaces using HTML, CSS, and JavaScript. If you have a strong eye for design and enjoy creating interactive web experiences, apply today!'
+  ],
+  [
+    'title' => 'Full Stack Developer',
+    'tags' => 'php, javascript, mysql',
+    'company' => 'Acme Corp',
+    'location' => 'San Francisco, CA',
+    'email' => 'developer3@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is seeking a talented Full Stack Developer to join our team in San Francisco, CA. The ideal candidate should have expertise in PHP, JavaScript, and MySQL. If you are passionate about building robust web applications and have experience working with both frontend and backend technologies, apply now!'
+  ],
+  [
+    'title' => 'React Developer',
+    'tags' => 'react, javascript, css',
+    'company' => 'Acme Corp',
+    'location' => 'Seattle, WA',
+    'email' => 'developer4@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is looking for a skilled React Developer to join our team in Seattle, WA. As a React Developer, you will be responsible for developing user interfaces using React, JavaScript, and CSS. If you have a strong understanding of frontend development and enjoy working with modern web technologies, apply today!'
+  ],
+  [
+    'title' => 'Full Stack Developer',
+    'tags' => 'php, javascript, mysql',
+    'company' => 'Tech Solutions Inc.',
+    'location' => 'San Francisco, CA',
+    'email' => 'fullstackdev@techsolutions.com',
+    'website' => 'https://www.techsolutions.com',
+    'description' => 'Join our team as a Full Stack Developer at Tech Solutions Inc. We are seeking a skilled developer proficient in PHP, JavaScript, and MySQL. You will be responsible for developing and maintaining web applications. If you are passionate about coding and enjoy working in a dynamic environment, apply now!'
+  ],
+  [
+    'title' => 'React Developer',
+    'tags' => 'react, javascript, css',
+    'company' => 'Digital Innovators',
+    'location' => 'New York, NY',
+    'email' => 'reactdev@digitalinnovators.com',
+    'website' => 'https://www.digitalinnovators.com',
+    'description' => 'Digital Innovators is hiring a React Developer to join our team in New York City. As a React Developer, you will be responsible for building user interfaces and implementing engaging web experiences. Proficiency in React, JavaScript, and CSS is required. If you have a strong passion for front-end development and want to work on cutting-edge projects, apply today!'
+  ],
+  [
+    'title' => 'Backend Developer',
+    'tags' => 'python, django, sql',
+    'company' => 'CodeCrafters Ltd.',
+    'location' => 'London, UK',
+    'email' => 'backenddev@codecrafters.com',
+    'website' => 'https://www.codecrafters.com',
+    'description' => 'CodeCrafters Ltd. is seeking a talented Backend Developer to join our team in London. As a Backend Developer, you will work on designing and implementing server-side logic, databases, and APIs. Proficiency in Python, Django, and SQL is required. If you are a problem solver with a strong background in backend development, apply now!'
+  ],
+  [
+    'title' => 'Mobile App Developer',
+    'tags' => 'android, kotlin, java',
+    'company' => 'AppGenius',
+    'location' => 'Los Angeles, CA',
+    'email' => 'mobiledev@appgenius.com',
+    'website' => 'https://www.appgenius.com',
+    'description' => 'AppGenius is looking for a talented Mobile App Developer to join our team in Los Angeles. As a Mobile App Developer, you will be responsible for developing and maintaining Android applications. Proficiency in Android, Kotlin, and Java is required. If you have a passion for mobile app development and want to work on innovative projects, apply today!'
+  ],
+  [
+    'title' => 'Frontend Developer',
+    'tags' => 'html, css, javascript',
+    'company' => 'WebTech Solutions',
+    'location' => 'Seattle, WA',
+    'email' => 'frontenddev@webtechsolutions.com',
+    'website' => 'https://www.webtechsolutions.com',
+    'description' => 'WebTech Solutions is seeking a skilled Frontend Developer to join our team in Seattle. As a Frontend Developer, you will be responsible for developing user interfaces and implementing interactive web designs. Proficiency in HTML, CSS, and JavaScript is required. If you have a strong eye for detail and a passion for creating engaging web experiences, apply now!'
+  ],
+  [
+    'title' => 'Java Developer',
+    'tags' => 'java, spring, hibernate',
+    'company' => 'ByteMasters',
+    'location' => 'Chicago, IL',
+    'email' => 'javadev@bytemasters.com',
+    'website' => 'https://www.bytemasters.com',
+    'description' => 'Join ByteMasters as a Java Developer in Chicago. As a Java Developer, you will work on developing and maintaining Java-based applications. Proficiency in Java, Spring, and Hibernate is required. If you are a problem solver with a strong background in Java development, apply today and be part of our innovative team!'
+  ],
+  [
+    'title' => 'Python Developer',
+    'tags' => 'python, django, sql',
+    'company' => 'Acme Corp',
+    'location' => 'Austin, TX',
+    'email' => 'developer5@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is seeking a talented Python Developer to join our team in Austin, TX. The ideal candidate should have expertise in Python, Django, and SQL. If you are passionate about backend development and have experience working with database-driven web applications, apply now!'
+  ],
+  [
+    'title' => 'Mobile App Developer',
+    'tags' => 'android, java, kotlin',
+    'company' => 'Acme Corp',
+    'location' => 'Los Angeles, CA',
+    'email' => 'developer6@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is looking for a talented Mobile App Developer to join our team in Los Angeles, CA. As a Mobile App Developer, you will be responsible for developing and maintaining Android applications using Java and Kotlin. If you have a passion for mobile app development and enjoy working in a fast-paced environment, apply today!'
+  ], [
+    'title' => 'UI/UX Designer',
+    'tags' => 'ui design, ux design, prototyping',
+    'company' => 'Acme Corp',
+    'location' => 'Chicago, IL',
+    'email' => 'developer7@acme.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'Acme Corp is seeking a skilled UI/UX Designer to join our team in Chicago, IL. The ideal candidate should have expertise in UI design, UX design, and prototyping. If you have a strong eye for aesthetics and enjoy creating intuitive user interfaces, apply now!'
+  ]
+];
